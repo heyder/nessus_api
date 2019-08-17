f976f05b345716db759a0c88cb800bee0d6c44a2
Heyder <heyder@devnull>
2019-08-18 01:29:22 +0200

    Test cases NessusApi itself.

--- lib/nessus_api.rb
+++ lib/nessus_api.rb
@@ -1,9 +1,12 @@
-
+# require 'pry'
 require_relative 'nessus_api/version'
+require_relative 'nessus_api/exception'
 
 Dir[File.join(__dir__, 'modules', '*.rb')].each { |file| require file }
 
 class NessusApi
+
+  attr_reader :request, :session
   
   include NessusApi::Scans
   include NessusApi::Exports
@@ -14,21 +17,27 @@ class NessusApi
   autoload :Session, "nessus_api/session"
 
   def initialize( params={uri: nil, username: nil, password: nil, :ssl_verify_peer => false} )
-
+    @has_session = false
     req_params = params.select {|key, value| [:uri, :ssl_verify_peer].include?(key) } 
     # session_params = params.select {|key, value| [:username, :password].include?(key) } 
 
-    @@request = NessusApi::Request.new( req_params )
-    api_session = NessusApi::Session.create( params.fetch(:username), params.fetch(:password) )
+    @request = NessusApi::Request.new( req_params )
+    @session = NessusApi::Session.create( params.fetch(:username), params.fetch(:password) )
     
-    if api_session.token
-      @has_session = true
-      NessusApi::Request.headers.update( 'X-Cookie' => 'token=' + api_session.token )
-    end
+    if @session.token
+      begin
+        @has_session = true
+        # NessusApi::Request.headers.update( 'X-Cookie' => 'token=' + api_session.token )
+        @request.headers.update( 'X-Cookie' => 'token=' + @session.token )
+        @session.set_api_token
+      rescue NessusApi::Error => err
+        puts err.message
+      else
+        request.headers.update( 'X-API-Token' => @session.api_token )
+      ensure
+        return
+      end
 
-    if has_session?
-      api_session.get_api_token
-      NessusApi::Request.headers.update( 'X-API-Token' => api_session.api_token )
     end
 
   end
@@ -38,7 +47,7 @@ class NessusApi
   end
 
   def status
-    NessusApi::Request.get( "/server/status" )
+    self.request.get( "/server/status" )
   end
 
 end
