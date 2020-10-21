9721bf9c0015de1021920a4509d66d9a2cf674af
Heyder <eu@eu>
2020-10-21 13:17:11 +0200

    small changes in the tests

--- spec/modules/policies_spec.rb
+++ spec/modules/policies_spec.rb
@@ -11,6 +11,9 @@ describe Resource::Policies do
     }
   end
   before :each do
+    @policy_name = 'policy_name'
+    @policy_id = 123
+
     @list_of_policies = Oj.load(
       Oj.dump(
         {
@@ -21,7 +24,7 @@ describe Resource::Policies do
               plugin_filters: nil,
               template_uuid: 'ad629e16-03b6-8c1d-cef6-ef8c9dd3c658d24bd260ef5f9e66',
               description: nil,
-              name: 'policy',
+              name: @policy_name,
               owner: 'user',
               visibility: 'private',
               shared: 0,
@@ -29,34 +32,39 @@ describe Resource::Policies do
               last_modification_date: 1_601_564_546,
               creation_date: 1_601_561_642,
               owner_id: 2,
-              id: 123 }
-]
+              id: @policy_id }
+          ]
         }, mode: :compat
       )
     )
+
+    @credential_id = '962'
+
+    @default_credential = { 
+      add: {},
+      edit:
+        { 
+          "#{@credential_id}":
+            {
+              username: 'default_username',
+              auth_method: 'Password',
+              password: 'default_password' }
+            },
+      delete: {}
+    }
 
     @policy = Oj.load(
       Oj.dump(
         {
-          credentials:
-            { add: {},
-              edit:
-              { "962":
-                { username: 'usernameasda2',
-                  auth_method: 'Password',
-                  password: 'password1' } },
-              delete: {} },
           plugins:
-              {},
+            {},
           settings:
-              @list_of_policies['policies'].first,
-          uuid:
-          {}
+            @list_of_policies['policies'].first,
+          uuid: {},
+          credentials: @default_credential
         }, mode: :compat
       )
     )
-
-    policy_id = @list_of_policies['policies'].first['id']
 
     allow_any_instance_of(NessusClient::Request).to receive(
       :get
@@ -64,37 +72,29 @@ describe Resource::Policies do
 
     allow_any_instance_of(NessusClient::Request).to receive(
       :get
-    ).with({ path: "/policies/#{policy_id}", headers: be_kind_of(Hash) }).and_return(@policy)
-
-    allow_any_instance_of(NessusClient::Request).to receive(
-      :put
-    ).with(
-      { path: "/policies/#{policy_id}",
-        headers: be_kind_of(Hash),
-        payload: be_kind_of(Hash) }
-    ).and_return(nil)
+    ).with({ path: "/policies/#{@policy_id}", headers: be_kind_of(Hash) }).and_return(@policy)
 
     allow_any_instance_of(Resource::Session).to receive(
       :set_session
     ).with('username', 'password').and_return('mock_auth_cookie')
+
     allow_any_instance_of(NessusClient).to receive(
       :new
     ).and_return(NessusClient.new(@payload))
 
     @nessus_client = NessusClient.new(@payload)
 
-    @policy_name = 'policy'
   end
 
   context '.policies' do
     it 'successful get all policies' do
       policies = @nessus_client.policies
       expect(policies).to have_key('policies')
-      expect(policies['policies'].first['id']).to eq(123)
+      expect(policies['policies'].first['id']).to eq(@policy_id)
     end
     it 'successful get a policy id by name' do
-      policy = @nessus_client.get_policy_id_by_name(@policy_name)
-      expect(policy).to eq(123)
+      policy_id = @nessus_client.get_policy_id_by_name(@policy_name)
+      expect(policy_id).to eq(@policy_id)
     end
 
     it 'successful get a policy by name' do
@@ -103,25 +103,36 @@ describe Resource::Policies do
     end
 
     it 'successful get all credentials of one policy' do
-      policy = @nessus_client.list_credentials_by_policy_name(@policy_name)
-      expect(policy).to have_key('credentials')
+      credentials = @nessus_client.list_credentials_by_policy_name(@policy_name)
+      # binding.pry
+      expect(credentials).to be_a_kind_of(Hash)
+      expect(credentials).to have_key('edit')
+      expect(credentials['edit']).to have_key(@credential_id)
     end
 
     it 'update a policy' do
-      payload = {
+      allow_any_instance_of(NessusClient::Request).to receive(
+        :put
+      ).with(
+        { path: "/policies/#{@policy_id}",
+          headers: be_kind_of(Hash),
+          payload: be_kind_of(Hash) }
+      ).and_return(nil)
+      new_credential = {
         "credentials": {
           "add": {},
           "edit": {
             "962": {
-              "username": 'usernameasda2',
+              "username": 'new_username',
               "auth_method": 'Password',
-              "password": 'password1'
+              "password": 'new_password'
             }
           },
           "delete": {}
         }
       }
-      policy = @nessus_client.update_policy_by_name(@policy_name, payload)
+      # TODO - test whether the policy has been updated.
+      policy = @nessus_client.update_policy_by_name(@policy_name, new_credential)
       expect(policy).to be_nil
     end
   end
