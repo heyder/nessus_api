f5eb245ec96e8b16c2f2a4e8a9b1b878e321495e
Heyder <eu@eu>
2020-10-21 13:29:20 +0200

    documentation update

--- lib/modules/policies.rb
+++ lib/modules/policies.rb
@@ -3,31 +3,31 @@
 # Namespace for Policies resource.
 module Resource::Policies
   # List the scan polices.
-  # @return [JSON]
+  # @return [JSON] list of policies.
   def policies
     request.get({ path: '/policies', headers: headers })
   end
 
-  # Get id of a policy.
+  # Get id of a policy by its name.
   # @param [String] policy name.
-  # @return policy_id
+  # @return [Integer] ID of a policy.
   def get_policy_id_by_name(policy_name)
     policies['policies'].each do |policy|
       return policy['id'] if policy['name'] == policy_name
     end
   end
 
-  # Get a policy.
+  # Get a policy by its name.
   # @param [String] policy name.
-  # @return [JSON]
+  # @return [JSON] policy object.
   def get_policy_by_name(policy_name)
     policy_id = get_policy_id_by_name(policy_name)
     request.get({ path: "/policies/#{policy_id}", headers: headers })
   end
 
-  # Get a list of credentials.
+  # Get a list of credentials from a policy.
   # @param [String] policy name.
-  # @return [JSON]
+  # @return [JSON] credential list
   def list_credentials_by_policy_name(policy_name)
     get_policy_by_name(policy_name)['credentials']
   end
