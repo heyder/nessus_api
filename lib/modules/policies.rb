27430ce41be15d2f9b993e7b2262889fb7790283
Heyder <eu@eu>
2020-10-22 11:30:14 +0200

    fix documentation

--- lib/modules/policies.rb
+++ lib/modules/policies.rb
@@ -3,13 +3,13 @@
 # Namespace for Policies resource.
 module Resource::Policies
   # List the scan polices.
-  # @return [JSON] list of policies.
+  # @return [Hash] list of policies.
   def policies
     request.get({ path: '/policies', headers: headers })
   end
 
   # Get id of a policy by its name.
-  # @param [String] policy name.
+  # @param [String] policy_name The of the policy.
   # @return [Integer] ID of a policy.
   def get_policy_id_by_name(policy_name)
     policies['policies'].each do |policy|
@@ -18,26 +18,26 @@ module Resource::Policies
   end
 
   # Get a policy by its name.
-  # @param [String] policy name.
-  # @return [JSON] policy object.
+  # @param [String] policy_name The of the policy.
+  # @return [Hash] A policy object.
   def get_policy_by_name(policy_name)
     policy_id = get_policy_id_by_name(policy_name)
     request.get({ path: "/policies/#{policy_id}", headers: headers })
   end
 
   # Get a list of credentials from a policy.
-  # @param [String] policy name.
-  # @return [JSON] credential list
+  # @param [String] policy_name The of the policy.
+  # @return [Hash] List of credentials
   def list_credentials_by_policy_name(policy_name)
     get_policy_by_name(policy_name)['credentials']
   end
 
   # update a scan policy.
-  # @param [String] policy name.
-  # @param [String] history_id (nil) The `history_id` of a scan.
+  # @param [String] policy_name The policy name.
+  # @param [Hash]  The policy fields to be updated.
   # @return nil
-  def update_policy_by_name(policy_name, payload)
+  def update_policy_by_name(policy_name, settings)
     id = get_policy_id_by_name(policy_name)
-    request.put({ path: "/policies/#{id}", headers: headers, payload: payload })
+    request.put({ path: "/policies/#{id}", headers: headers, payload: settings })
   end
 end
--- lib/modules/server.rb
+++ lib/modules/server.rb
@@ -3,13 +3,13 @@
 # Namespace for Server resource.
 module Resource::Server
   # Returns the server status.
-  # @return [JSON] Returns the server status
+  # @return [Hash] Returns the server status
   def server_status
     request.get({ path: '/server/status', headers: headers })
   end
 
   # Returns the server version and other properties.
-  # @return [JSON] Returns the server properties
+  # @return [Hash] Returns the server properties
   def server_properties
     request.get({ path: '/server/properties', headers: headers })
   end
